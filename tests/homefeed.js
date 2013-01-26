1db811085b5746f126fba8da6e2a49a4de408b70
Thomas Lackner <lackner@gmail.com>
2013-01-26 12:48:36 -0600

    login() and homeFeed() added, test script

---
+++ tests/homefeed.js
@@ -0,0 +1,24 @@
+var vino = require('../vino.js'), client;
+
+if (process.argv.length != 4) {
+	console.error('usage: node login.js [username] [password]');
+	process.exit(1);
+}
+
+	
+client = new vino({username: process.argv[2], password: process.argv[3]});
+client.login(function(err, key, username) {
+	
+	if (err) throw new Error(err);
+	console.log('successfully logged in, key and username: ', key, username);
+
+	client.homeFeed(function(err, feed) {
+		if (err) throw new Error(err);
+		// console.log('your timeline', feed);
+		for (var i in feed.records) {
+			var entry = feed.records[i];
+			console.log(entry.description, entry.videoUrl);
+		}
+	});
+
+});
