e92290c514eabea075c27455e1620a18549187a4
Thomas Lackner <lackner@gmail.com>
2013-01-26 11:55:34 -0600

    first checkin of base code

---
+++ README.md
@@ -0,0 +1,6 @@
+Vino
+====
+
+Vino is a Vine.co API client written in JavaScript
+
+
---
+++ vino.js
@@ -0,0 +1,11 @@
+if (typeof request == 'undefined') {
+	var request = require('request');
+}
+
+function Vino(options) {
+}
+
+Vino.prototype.login = function() {
+
+}
+
